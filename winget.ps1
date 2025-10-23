ab518084747f2c9f84c3ae2affd60678bda77aca
Tanil Izquierdo <32084752+Peloponeso31@users.noreply.github.com>
2025-10-23 00:55:14 -0600

    Comentarios explicativos del script.

--- winget.ps1
+++ winget.ps1
@@ -14,21 +14,26 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     exit 1
 }
 
+# Se crea el directorio donde se van a descargar los paquetes.
 if (!(Test-Path $install_path)) {
     New-Item -ItemType Directory -Path $install_path -Force | Out-Null
 }
 
+# Se obtienen los enlaces de descarga del ultimo release de winget.
 $release = Invoke-Restmethod -Uri https://api.github.com/repos/microsoft/winget-cli/releases/latest
 $bundle = $release.assets | Where-Object { $_.name -like "*.msixbundle" }
 $license = $release.assets | Where-Object { $_.name -like "*.xml" }
 $dependencies = $release.assets | Where-Object { $_.name -like "*dependencies.zip" }
 
+# Se utilizan dichos enlaces para descargar los archivos correspondientes.
 Invoke-WebRequest -Uri $bundle.browser_download_url -Outfile "$install_path\bundle.msixbundle"
 Invoke-WebRequest -Uri $license.browser_download_url -Outfile "$install_path\license.xml"
 Invoke-WebRequest -Uri $dependencies.browser_download_url -Outfile "$install_path\dependencies.zip"
 
+# Se extraen las dependencias.
 Expand-Archive -Path "$install_path\dependencies.zip" -DestinationPath "$install_path\dependencies"
 
+# Se instalan todos los paquetes y el mismo winget. Se limpia todo.
 Add-AppxPackage "$install_path\dependencies\x64\Microsoft.UI.Xaml*.appx"
 Add-AppxPackage "$install_path\dependencies\x64\Microsoft.VCLibs*.appx"
 Add-AppxProvisionedPackage -Online -PackagePath "$install_path\bundle.msixbundle" -LicensePath "$install_path\license.xml" -Verbose
