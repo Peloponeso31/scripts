1cd6e2c4dff63b8bb4ca2ae12fa54abaf6d47bdb
Tanil Izquierdo <32084752+Peloponeso31@users.noreply.github.com>
2025-12-30 23:48:41 -0600

    Update programs list in programs.ps1

--- programs.ps1
+++ programs.ps1
@@ -1,39 +1,37 @@
 # Array of Winget IDs for programs to install
 $programs = @(
+	"Zen-Team.Zen-Browser",
+	
 	"9MSMLRH6LZF3", # Notepad
 	"9MZ95KL8MR0L", # Snipping tool
 	"9P8LTPGCBZXD", # Wintoys
 	"9WZDNCRFJBH4", # Microsoft photos
 	
 	"BlenderFoundation.Blender",
-	"Brave.Brave",
+	"ImputNet.Helium",
 	"CharlesMilette.TranslucentTB",
 	"Discord.Discord",
 	"Git.Git",
-	#"HandBrake.HandBrake",
+	"HandBrake.HandBrake",
 	"Httpie.Httpie",
 	"Jetbrains.toolbox",
 	"KDE.KDEConnect",
 	"KDE.Krita",
 	"M2team.nanazip",
-	#"Microsoft.DirectX",
 	"Microsoft.DotNet.Runtime.9",
 	"Microsoft.VisualStudioCode",
 	"Microsoft.WindowsTerminal",
 	"Microsoft.powershell",
-	"Mojang.MinecraftLauncher",
 	"MoritzBunkus.MKVToolNix",
 	"Neovim.Neovim",
 	"OBSProject.OBSStudio",
 	"REALiX.HWiNFO",
-	#"Rem0o.FanControl",
+	"Rem0o.FanControl",
 	"SamHocevar.Wincompose",
 	"Starship.Starship",
 	"Transmission.Transmission",
-	#"Valve.steam",
-	"Videolan.vlc",
-	"Zen-Team.Zen-Browser"
-	#"GOG.Galaxy",
+	"Valve.steam",
+	"Videolan.vlc"
 )
 
 # Loop through each program and install it using winget
